a6eca269ecb20f33cc6dade38835bb8f6bd4b609
E Pruitt <epruitt223@gmail.com>
2026-09-04 13:08:15 -0500

    feat: added provider link

--- vpcWebsiteHosting-bootstrap/outputs.tf
+++ vpcWebsiteHosting-bootstrap/outputs.tf
@@ -12,3 +12,8 @@ output "github_oidc_provider_arn" {
   description = "ARN of the GitHub OIDC Identity Provider"
   value       = aws_iam_openid_connect_provider.github.arn
 }
+
+output "github_oidc_provider_url" {
+  description = "URL of the GitHub OIDC Identity Provider"
+  value       = aws_iam_openid_connect_provider.github.url
+}
