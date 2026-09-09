62f68c8201dda72c9bd4d60fa15f9fdb19bbf0f3
E Pruitt <epruitt223@gmail.com>
2026-09-08 20:28:52 -0500

    formatted file correctly

--- vpcWebsiteHosting/c7-github-oidc.tf
+++ vpcWebsiteHosting/c7-github-oidc.tf
@@ -278,7 +278,7 @@ data "aws_iam_policy_document" "plan_permissions" {
     sid = "TerraformStateAccess"
     actions = [
       "s3:GetObject",
-      "s3:PutObject", # Needed for lockfile
+      "s3:PutObject",    # Needed for lockfile
       "s3:DeleteObject", # Needed to release the lockfile (S3 native locking)
       "s3:ListBucket"
     ]
